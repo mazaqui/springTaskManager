3e35b63209f3c8c944a528c2786677db167fc072
Alexandre GINESY <aginesy@ippon.fr>
2023-10-26 18:26:10 +0200

    Added Gitignore

---
+++ gestion-taches/src/main/java/com/votrepackage/SpingTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.votrepackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingTaskManagerApplication.class, args);
+    }
+}
+
